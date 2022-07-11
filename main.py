ac9ee39d8ca67b201d288b9be6d20f8152fc60c7
Harry Negus <harrynegus@hotmail.co.uk>
2022-07-11 08:47:32 +0100

    Deploy

--- main.py
+++ main.py
@@ -8,71 +8,75 @@ import tweepy
 import time
 load_dotenv()
 
-weather_key = os.environ.get("WEATHER_KEY")
-twitter_consumer_key = os.environ.get("TWITTER_KEY")
-twitter_consumer_secret = os.environ.get("TWITTER_SECRET")
-twitter_access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
-twitter_access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+def run():
+    weather_key = os.environ.get("WEATHER_KEY")
+    twitter_consumer_key = os.environ.get("TWITTER_KEY")
+    twitter_consumer_secret = os.environ.get("TWITTER_SECRET")
+    twitter_access_token = os.environ.get("TWITTER_ACCESS_TOKEN")
+    twitter_access_token_secret = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
 
-auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
-auth.set_access_token(twitter_access_token, twitter_access_token_secret)
+    auth = tweepy.OAuthHandler(twitter_consumer_key, twitter_consumer_secret)
+    auth.set_access_token(twitter_access_token, twitter_access_token_secret)
 
-api = tweepy.API(auth)
+    api = tweepy.API(auth)
 
-data = None
-with open('data/city.list.json', encoding='utf-8') as cities:
-    data = json.load(cities)
+    data = None
+    with open('data/city.list.json', encoding='utf-8') as cities:
+        data = json.load(cities)
 
-countries_list = None
-with open('data/countries.json', encoding='utf-8') as countries:
-    countries_list = json.load(countries)
+    countries_list = None
+    with open('data/countries.json', encoding='utf-8') as countries:
+        countries_list = json.load(countries)
 
-while True:
-    def get_potential_rainy_cities():
-        cities = []
-        for x in range(20):
-            cities.append(data[random.randrange(0, len(data) - 1)]) 
+    while True:
+        def get_potential_rainy_cities():
+            cities = []
+            for x in range(20):
+                cities.append(data[random.randrange(0, len(data) - 1)]) 
 
-        url = f'http://api.openweathermap.org/data/2.5/group?id={",".join(map(lambda city: str(city["id"]), cities))}&units=metric&appid={weather_key}'
-        response = requests.get(url)
-        return response.json()['list']
+            url = f'http://api.openweathermap.org/data/2.5/group?id={",".join(map(lambda city: str(city["id"]), cities))}&units=metric&appid={weather_key}'
+            response = requests.get(url)
+            return response.json()['list']
 
-    def is_it_raining(weather):
-        weather_code = str(weather['weather'][0]['id'])
-        weather_code_first_digit = weather_code[0]
-        if weather_code_first_digit == '5':
-            return True
-        if weather_code_first_digit == '3':
-            return True
-        if weather_code == '200':
-            return True
-        if weather_code == '201':
-            return True
-        if weather_code == '202':
-            return True
-        if weather_code == '230':
-            return True
-        if weather_code == '231':
-            return True
-        if weather_code == '232':
-            return True
-        return False
-    
-    found_potential_rainy_cities = False
-    times_looked = 0
-    where_its_raining = None
-    while found_potential_rainy_cities is False:
-        if times_looked >= 3:
-            print("cannot find any rainy cities... will look again.")
-            time.sleep(60)#To not annoy the API
-            times_looked = 0
-        where_its_raining = list(filter(is_it_raining, get_potential_rainy_cities()))
-        times_looked += 1
-        if len(where_its_raining) > 0:
-            found_potential_rainy_cities = True
+        def is_it_raining(weather):
+            weather_code = str(weather['weather'][0]['id'])
+            weather_code_first_digit = weather_code[0]
+            if weather_code_first_digit == '5':
+                return True
+            if weather_code_first_digit == '3':
+                return True
+            if weather_code == '200':
+                return True
+            if weather_code == '201':
+                return True
+            if weather_code == '202':
+                return True
+            if weather_code == '230':
+                return True
+            if weather_code == '231':
+                return True
+            if weather_code == '232':
+                return True
+            return False
+        
+        found_potential_rainy_cities = False
+        times_looked = 0
+        where_its_raining = None
+        while found_potential_rainy_cities is False:
+            if times_looked >= 3:
+                print("cannot find any rainy cities... will look again.")
+                time.sleep(60)#To not annoy the API
+                times_looked = 0
+            where_its_raining = list(filter(is_it_raining, get_potential_rainy_cities()))
+            times_looked += 1
+            if len(where_its_raining) > 0:
+                found_potential_rainy_cities = True
 
-    rainy_city = where_its_raining[0]
+        rainy_city = where_its_raining[0]
 
 
-    api.update_status(f'It\'s raining in {rainy_city["name"]}, {countries_list[rainy_city["sys"]["country"]]}. https://www.youtube.com/watch?v=zNd4apsr3WE')
-    time.sleep(3600)
+        api.update_status(f'It\'s raining in {rainy_city["name"]}, {countries_list[rainy_city["sys"]["country"]]}. https://www.youtube.com/watch?v=zNd4apsr3WE')
+        time.sleep(3600)
+
+if __name__ == "__main__":
+    run()
